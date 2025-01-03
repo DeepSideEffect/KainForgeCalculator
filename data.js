3703346168ebadeb89ea0d5ee55a22d0217d5bec
Islem BEZZARGA <islem.bezzarga-ext@ldc.com>
2025-01-03 20:01:05 +0100

    structuration

---
+++ data.js
@@ -0,0 +1,30 @@
+const typesObjets = [
+    { id: 1, nom: "Les casques" },
+    { id: 2, nom: "Les armures" },
+    { id: 3, nom: "Les pantalons" },
+    { id: 4, nom: "Les amulettes" },
+    { id: 5, nom: "Les anneaux" },
+    { id: 6, nom: "Les armes à feu à 1 main" },
+    { id: 7, nom: "Les armes à feu à 2 mains" },
+    { id: 8, nom: "Les Armes À Distance" },
+    { id: 9, nom: "Les armes de Corps À Corps à 1 Main" },
+    { id: 10, nom: "Les armes de Corps À Corps à 2 Mains" }
+];
+
+const caracteristiquesSupport = [
+    new Caracteristique("Support1", 10),
+    new Caracteristique("Support2", 15),
+    new Caracteristique("Support3", 20)
+];
+
+const caracteristiquesPrefixe = [
+    new Caracteristique("Prefixe1", 5),
+    new Caracteristique("Prefixe2", 8),
+    new Caracteristique("Prefixe3", 12)
+];
+
+const caracteristiquesSuffixe = [
+    new Caracteristique("Suffixe1", 7),
+    new Caracteristique("Suffixe2", 12),
+    new Caracteristique("Suffixe3", 18)
+];
