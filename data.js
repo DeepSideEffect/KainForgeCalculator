5732d2d2864e8e212ad10e47b5c5651b1873b969
Deep <193667101+DeepSideEffect@users.noreply.github.com>
2025-01-03 21:17:28 +0100

    préparation

--- data.js
+++ data.js
@@ -1,3 +1,4 @@
+// Les types d'objets
 const typesObjets = [
     { id: 1, nom: "Les casques" },
     { id: 2, nom: "Les armures" },
@@ -11,23 +12,45 @@ const typesObjets = [
     { id: 10, nom: "Les armes de Corps À Corps à 2 Mains" }
 ];
 
+// Les casques
+const casqueCaracteristiques = {
+	support: [
+		new Caracteristique("Support1", 10),
+		new Caracteristique("Support2", 15),
+		new Caracteristique("Support3", 20)
+	],
+	prefixe: [
+		new Caracteristique("Prefixe1", 5),
+		new Caracteristique("Prefixe2", 8),
+		new Caracteristique("Prefixe3", 12)
+	],
+	suffixe: [
+		new Caracteristique("Suffixe1", 7),
+		new Caracteristique("Suffixe2", 12),
+		new Caracteristique("Suffixe3", 18)
+	]
+};
+
+// Les armures
+
+// Les pantalons
+
+// Les amulettes
+
+// Les anneaux
+
+// Les armes à feu à 1 main
+
+// Les armes à feu à 2 mains
+
+// Les Armes À Distance
+
+// Les armes de Corps À Corps à 1 Main
+
+// Les armes de Corps À Corps à 2 Mains
+
+// Forge de Kain
 const caracteristiques = {
-    1: {
-        support: [
-            new Caracteristique("Support1", 10),
-            new Caracteristique("Support2", 15),
-            new Caracteristique("Support3", 20)
-        ],
-        prefixe: [
-            new Caracteristique("Prefixe1", 5),
-            new Caracteristique("Prefixe2", 8),
-            new Caracteristique("Prefixe3", 12)
-        ],
-        suffixe: [
-            new Caracteristique("Suffixe1", 7),
-            new Caracteristique("Suffixe2", 12),
-            new Caracteristique("Suffixe3", 18)
-        ]
-    },
-    // Ajoute les caractéristiques pour les autres types d'objets ici
+    1: casqueCaracteristiques,
+    // TODO: Ajoute les caractéristiques pour les autres types d'objets ici
 };
