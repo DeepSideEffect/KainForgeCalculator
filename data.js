b24d220250db53f8ea0a6cbded7d852c37c77493
Deep <193667101+DeepSideEffect@users.noreply.github.com>
2025-01-04 10:42:26 +0100

    v1.0.1 | épuration + style + évolutions

--- data.js
+++ data.js
@@ -7,9 +7,9 @@ const typesObjets = [
     { id: 5, nom: "Les anneaux", piecesEpiques: 20, nbRunes: 1, libelleRunes: "Bleue Niveau 2", color: "#2194b5" },
     { id: 6, nom: "Les armes à feu à 1 main", piecesEpiques: 50, nbRunes: 1, libelleRunes: "Jaune Niveau 4", color: "#b5b121" },
     { id: 7, nom: "Les armes à feu à 2 mains", piecesEpiques: 100, nbRunes: 2, libelleRunes: "Jaunes Niveau 4", color: "#b5b121" },
-    { id: 8, nom: "Les Armes À Distance", piecesEpiques: 75, nbRunes: 2, libelleRunes: "Rouges Niveau 3", color: "#b53221" },
-    { id: 9, nom: "Les armes de Corps À Corps à 1 Main", piecesEpiques: 20, nbRunes: 1, libelleRunes: "Rouge Niveau 2", color: "#b53221" },
-    { id: 10, nom: "Les armes de Corps À Corps à 2 Mains", piecesEpiques: 40, nbRunes: 2, libelleRunes: "Rouges Niveau 2", color: "#b53221" }
+    { id: 8, nom: "Les armes à distance", piecesEpiques: 75, nbRunes: 2, libelleRunes: "Rouges Niveau 3", color: "#b53221" },
+    { id: 9, nom: "Les armes de corps à corps à 1 main", piecesEpiques: 20, nbRunes: 1, libelleRunes: "Rouge Niveau 2", color: "#b53221" },
+    { id: 10, nom: "Les armes de corps à corps à 2 mains", piecesEpiques: 40, nbRunes: 2, libelleRunes: "Rouges Niveau 2", color: "#b53221" }
 ];
 
 //#region Les casques
