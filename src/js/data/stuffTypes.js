c8171d6c7dd800af42eb600482833d5a4393d69c
Deep <193667101+DeepSideEffect@users.noreply.github.com>
2025-01-06 10:47:36 +0100

    Ajout .editorconfig + correction indentation du code

--- src/js/data/stuffTypes.js
+++ src/js/data/stuffTypes.js
@@ -1,13 +1,13 @@
 // Les types d'objets
 const typesObjets = [
-    { id: 1, nom: "Les casques", piecesEpiques: 15, nbRunes: 1, libelleRunes: "Verte Niveau 2", color: "#21b530" },
-    { id: 2, nom: "Les armures", piecesEpiques: 15, nbRunes: 1, libelleRunes: "Verte Niveau 2", color: "#21b530" },
-    { id: 3, nom: "Les pantalons", piecesEpiques: 15, nbRunes: 1, libelleRunes: "Verte Niveau 2", color: "#21b530" },
-    { id: 4, nom: "Les amulettes", piecesEpiques: 40, nbRunes: 1, libelleRunes: "Bleue Niveau 2", color: "#2194b5" },
-    { id: 5, nom: "Les anneaux", piecesEpiques: 20, nbRunes: 1, libelleRunes: "Bleue Niveau 2", color: "#2194b5" },
-    { id: 6, nom: "Les armes à feu à 1 main", piecesEpiques: 50, nbRunes: 1, libelleRunes: "Jaune Niveau 4", color: "#b5b121" },
-    { id: 7, nom: "Les armes à feu à 2 mains", piecesEpiques: 100, nbRunes: 2, libelleRunes: "Jaunes Niveau 4", color: "#b5b121" },
-    { id: 8, nom: "Les armes à distance", piecesEpiques: 75, nbRunes: 2, libelleRunes: "Rouges Niveau 3", color: "#b53221" },
-    { id: 9, nom: "Les armes de corps à corps à 1 main", piecesEpiques: 20, nbRunes: 1, libelleRunes: "Rouge Niveau 2", color: "#b53221" },
-    { id: 10, nom: "Les armes de corps à corps à 2 mains", piecesEpiques: 40, nbRunes: 2, libelleRunes: "Rouges Niveau 2", color: "#b53221" }
+	{ id: 1, nom: "Les casques", piecesEpiques: 15, nbRunes: 1, libelleRunes: "Verte Niveau 2", color: "#21b530" },
+	{ id: 2, nom: "Les armures", piecesEpiques: 15, nbRunes: 1, libelleRunes: "Verte Niveau 2", color: "#21b530" },
+	{ id: 3, nom: "Les pantalons", piecesEpiques: 15, nbRunes: 1, libelleRunes: "Verte Niveau 2", color: "#21b530" },
+	{ id: 4, nom: "Les amulettes", piecesEpiques: 40, nbRunes: 1, libelleRunes: "Bleue Niveau 2", color: "#2194b5" },
+	{ id: 5, nom: "Les anneaux", piecesEpiques: 20, nbRunes: 1, libelleRunes: "Bleue Niveau 2", color: "#2194b5" },
+	{ id: 6, nom: "Les armes à feu à 1 main", piecesEpiques: 50, nbRunes: 1, libelleRunes: "Jaune Niveau 4", color: "#b5b121" },
+	{ id: 7, nom: "Les armes à feu à 2 mains", piecesEpiques: 100, nbRunes: 2, libelleRunes: "Jaunes Niveau 4", color: "#b5b121" },
+	{ id: 8, nom: "Les armes à distance", piecesEpiques: 75, nbRunes: 2, libelleRunes: "Rouges Niveau 3", color: "#b53221" },
+	{ id: 9, nom: "Les armes de corps à corps à 1 main", piecesEpiques: 20, nbRunes: 1, libelleRunes: "Rouge Niveau 2", color: "#b53221" },
+	{ id: 10, nom: "Les armes de corps à corps à 2 mains", piecesEpiques: 40, nbRunes: 2, libelleRunes: "Rouges Niveau 2", color: "#b53221" }
 ];
--- src/js/script/appBusiness.js
+++ src/js/script/appBusiness.js
@@ -6,29 +6,29 @@
  * @returns Le coût de la modification pour la caractéristique donnée
  */
 function calculerCoutCaracteristique(actuel, souhaite, caracteristiques) {
-    if (actuel?.nom === souhaite?.nom) {
-        return { coutPE: 0, nbMvts: 0 };
-    }
+	if (actuel?.nom === souhaite?.nom) {
+		return { coutPE: 0, nbMvts: 0 };
+	}
 
-    const indexActuel = caracteristiques.findIndex(c => c.nom === actuel.nom);
-    const indexSouhaite = caracteristiques.findIndex(c => c.nom === souhaite.nom);
+	const indexActuel = caracteristiques.findIndex(c => c.nom === actuel.nom);
+	const indexSouhaite = caracteristiques.findIndex(c => c.nom === souhaite.nom);
 
-    let nbMvts = 0;
-    let coutPE = 0;
+	let nbMvts = 0;
+	let coutPE = 0;
 
-    if (indexActuel < indexSouhaite) {
-        for (let i = indexActuel + 1; i <= indexSouhaite; i++) {
-            coutPE += caracteristiques[i].pe;
-            nbMvts++;
-        }
-    } else {
-        for (let i = indexActuel - 1; i >= indexSouhaite; i--) {
-            coutPE += caracteristiques[i].pe;
-            nbMvts++;
-        }
-    }
+	if (indexActuel < indexSouhaite) {
+		for (let i = indexActuel + 1; i <= indexSouhaite; i++) {
+			coutPE += caracteristiques[i].pe;
+			nbMvts++;
+		}
+	} else {
+		for (let i = indexActuel - 1; i >= indexSouhaite; i--) {
+			coutPE += caracteristiques[i].pe;
+			nbMvts++;
+		}
+	}
 
-    return { coutPE, nbMvts };
+	return { coutPE, nbMvts };
 }
 
 /**
@@ -41,22 +41,22 @@ function calculerCoutCaracteristique(actuel, souhaite, caracteristiques) {
  * @returns Le coût total de la modification d'un objet donné
  */
 function calculerCoutTotal(objetActuel, objetSouhaite, caracteristiquesSupport, caracteristiquesPrefixe, caracteristiquesSuffixe) {
-    let coutTotalPE = 0;
-    let nbMvtsTotal = 0;
+	let coutTotalPE = 0;
+	let nbMvtsTotal = 0;
 
 	const resultatSupport = calculerCoutCaracteristique(objetActuel.support, objetSouhaite.support, caracteristiquesSupport);
 	const resultatPrefixe = calculerCoutCaracteristique(objetActuel.prefixe, objetSouhaite.prefixe, caracteristiquesPrefixe);
 	const resultatSuffixe = calculerCoutCaracteristique(objetActuel.suffixe, objetSouhaite.suffixe, caracteristiquesSuffixe);
 
-    coutTotalPE += resultatSupport.coutPE;
-    coutTotalPE += resultatPrefixe.coutPE;
-    coutTotalPE += resultatSuffixe.coutPE;
+	coutTotalPE += resultatSupport.coutPE;
+	coutTotalPE += resultatPrefixe.coutPE;
+	coutTotalPE += resultatSuffixe.coutPE;
 
 	nbMvtsTotal += resultatSupport.nbMvts;
 	nbMvtsTotal += resultatPrefixe.nbMvts;
 	nbMvtsTotal += resultatSuffixe.nbMvts;
 
-    return { coutTotalPE, nbMvtsTotal };
+	return { coutTotalPE, nbMvtsTotal };
 }
 
 /**
@@ -65,20 +65,20 @@ function calculerCoutTotal(objetActuel, objetSouhaite, caracteristiquesSupport, 
  * @returns le nom complet au format texte
  */
 function nomCompletObjet(objet) {
-    return `Epique ${objet?.support?.nom ?? ''} ${objet?.prefixe?.nom ?? ''} ${objet?.suffixe?.nom ?? ''}`;
+	return `Epique ${objet?.support?.nom ?? ''} ${objet?.prefixe?.nom ?? ''} ${objet?.suffixe?.nom ?? ''}`;
 }
 
 /** Nettoyage des doubles sauts de ligne */
 function replaceDoubleCRLF(text) {
-    return text.replace(/\n\n/g, '\n');
+	return text.replace(/\n\n/g, '\n');
 }
 
 function ajouteSecifiquesDoubleCRLF(text) {
-    return text.replace("Coût total :", "\n\nCoût total :");
+	return text.replace("Coût total :", "\n\nCoût total :");
 }
 
 function gererLesCRLF(text) {
-    text = replaceDoubleCRLF(text);
-    text = ajouteSecifiquesDoubleCRLF(text);
-    return text;
+	text = replaceDoubleCRLF(text);
+	text = ajouteSecifiquesDoubleCRLF(text);
+	return text;
 }
